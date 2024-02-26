43de594168730c5884b8c525b47b72911086550a
heather frank <sjcswank@gmail.com>
2024-02-26 13:54:59 -0600

    Added Document classes for Movie and Review

---
+++ src/main/java/sjcswank/movies/Review.java
@@ -0,0 +1,18 @@
+package sjcswank.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    private ObjectId id;
+    private String body;
+
+}
